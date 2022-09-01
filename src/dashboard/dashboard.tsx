367581156ba4d34c33557846387da1a9f9acc924
Yowsif Ahamed <yowsifahamed@gmail.com>
2022-09-01 14:24:29 +0530

    dashboard json added

--- src/dashboard/dashboard.tsx
+++ src/dashboard/dashboard.tsx
@@ -1,12 +1,45 @@
 import React from "react";
-import { Footer } from "../footer/footer";
-import { Header } from "../header/header";
-
+import JSONQUIZ from '../assets/db/quizCollection.json'
+import './dashboard.scss';
 export class Dashboard extends React.Component {
-    render() {
-      return <div>
-        <Header></Header>
-        <Footer></Footer>
+  public dashboardData = JSONQUIZ.quizCol
+
+  constructor(props: any) {
+    super(props);
+    // this.state = {
+    //   dashboardValues: JSONQUIZ.quizCol || []
+    // };
+  }
+
+  componentWillMount(): void {
+    // console.log("data",this.state)
+  }
+
+  componentDidUpdate() {
+
+  }
+
+
+  render() {
+    return <div className="dashboard">
+      <div className="row">
+        {this.dashboardData.map((item, i) => (
+          <div className="col-sm-12 col-md-4 das-col-sm">
+            <div className="img-section">
+              <img className="content-img" alt="quiz app" src={`${item.col}`} />
+              <div className="image-header">
+                <span className="category"> {item.name} </span>
+              </div>
+              <div className="image-footer">
+                <div className="quiz-time">
+                  <span className="Home_nowPlaying__2zAVk">👤 8 quizzing now...</span>
+                  <div className="Home_quizDuration__3tH8c">🏷️ 20Q / ⏱️ 8min</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-    }
+    </div>
+  }
 }
