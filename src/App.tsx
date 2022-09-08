a4cf4f03d7cb9f4f0794a370d7bfafd828bdf0cc
Yowsif Ahamed <yowsifahamed@gmail.com>
2022-09-08 23:08:23 +0530

    header hiding logic added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { ReactFragment, ReactPortal, useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes, useLocation, useParams } from 'react-router-dom';
 import './App.css';
 import Column from './column/column';
 import { Dashboard } from './dashboard/dashboard';
@@ -8,12 +8,21 @@ import { Header } from './header/header';
 
 
 function App() {
+  const [showNav, setShowNav] = useState(false);
+
+  useEffect(() => {
+    if (window.location.pathname == "/") {
+      setShowNav(true)
+    }else{
+      setShowNav(false)
+    }
+  });
 
   return (
     <>
       <BrowserRouter>
         <div className="App">
-          <Header></Header>
+          { showNav && <Header/> }
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="dashboard" element={<Dashboard />} />
